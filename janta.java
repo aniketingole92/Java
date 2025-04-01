b548abde19697da27cb2346489863ffdcedd93b6
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-01 19:26:52 +0530

    Create janta.java

---
+++ janta.java
@@ -0,0 +1,15 @@
+class nest 
+{
+	public static void main(String[] args)
+	{
+		int token = 30;
+		char softDrink = 'S';
+		if (token == 30){
+			System.out.println("Collect sprite");
+		} else if(softDrink == 'F'){
+			System.out.println("Collect fanta");
+		} else {
+			System.out.println("Invalid options");
+		}
+	}
+}
