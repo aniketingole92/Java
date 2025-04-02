7837ec47cb52d0e3b2a8e161cb7e6eb334ec72b2
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-02 13:34:14 +0530

    Create whiile.java

---
+++ whiile.java
@@ -0,0 +1,15 @@
+public class whille {
+    public static void main(String[] args) {
+        int i = 1;
+        
+        while (i <= 4) {
+
+            System.out.println("Hello Aniket " + i);
+            i++;
+
+            
+        }
+    }
+}
+
+
