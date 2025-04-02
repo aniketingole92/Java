da761ade7391e1c742bd71850ebb98c9a8afe263
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-02 12:57:22 +0530

    Create elsee.java

---
+++ elsee.java
@@ -0,0 +1,13 @@
+public class elsee {
+    public static void main(String[] args) {
+        int n = 4;
+        int result = 0 ;
+
+        if (n % 2==0)
+            result = 10;
+        else 
+            result = 20;
+        System.out.println(result);
+
+    }
+}
