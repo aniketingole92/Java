391a17cda9e10388250b84a70f4e6b903f616253
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-02 12:57:00 +0530

    Create ternary.java

---
+++ ternary.java
@@ -0,0 +1,12 @@
+public class ternary {
+    public static void main(String[] args) {
+        
+        int n  = 4;
+        int reult = 0;
+
+        reult = n % 2 ==0 ? 10 : 20;
+
+
+        System.out.println(reult);
+    }
+}
