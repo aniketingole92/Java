720432303ef1c7f96561bd53d84083de452b46ca
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-02 13:34:40 +0530

    Create whilee.java

---
+++ whilee.java
@@ -0,0 +1,13 @@
+public class whilee {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (true) {
+
+            System.out.println("Aniket " + i);
+
+            i++;
+            
+        }
+    }
+}
