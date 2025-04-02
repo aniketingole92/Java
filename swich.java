6182c47bf64c11ab37d8e5cea4394f08976a00dc
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-02 12:56:30 +0530

    Create swich.java

---
+++ swich.java
@@ -0,0 +1,35 @@
+public class swich {
+    
+    public static void main(String[] args) {
+        int n = 9;
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tusday");
+                break;
+            case 3:
+                System.out.println("Wenesday");
+                break;
+            case 4:
+                System.out.println("Thusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+
+        
+            default:
+                System.out.println("Enter Invalid number");
+
+        }
+    }
+}
