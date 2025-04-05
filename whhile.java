3c73be9c54c3f15203b53a6bb4e935a1bd94b215
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-05 10:46:00 +0530

    Add files via upload

---
+++ whhile.java
@@ -0,0 +1,16 @@
+public class whhile {
+
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (true) {
+            System.out.println("Hello Sir " + i);
+
+            i++;
+
+
+            
+        }
+    }
+}
+
