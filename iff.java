36fe97333fa69b144112c29ef4e2abde357eba10
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-05 11:16:26 +0530

    Add files via upload

--- iff.java
+++ iff.java
@@ -1,16 +1,11 @@
 public class iff {
-
     public static void main(String[] args) {
-        int x = 9 ;
-        int y = 7 ;
-        int z = 6;
+        int x = 18;
 
-        if (x > y && x > z)
-            System.out.println(x);
-        else if (y > x && y>z)
-            System.out.println(y);
-        else 
-            System.out.println(z);
+        if (false)
+        System.out.println("Hello");
+
+
 
     }
 }
