9018052a4ebf767580b440d584b241b5d0a8a861
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-10 19:43:57 +0530

    Add files via upload

---
+++ bytee.java
@@ -0,0 +1,9 @@
+class bytee
+{
+	public static void main(String [] args)
+	{
+		byte a = 100;
+		a = a + a;
+		System.out.println(a);
+	}
+}
---
+++ condi.java
@@ -0,0 +1,15 @@
+class condi 
+{
+	public static void main(String [] args)
+	{
+		int marks = 100 ;
+		if (marks >= 90){
+			System.out.println("Topper");
+		
+		} else if ( marks <= 50 ){
+			System.out.println("Medium");
+		} else if (marks >= 100) {
+			System.out.println("Fail");
+		}
+	}
+}
