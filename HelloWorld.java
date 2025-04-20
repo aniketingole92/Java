19ed5784c358d2ce7a6f48c1dd66c9e5ae26efc8
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-20 22:53:27 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+// Simple Java program structure
+public class HelloWorld {
+    // Main method - entry point
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Print statement
+        
+        // Variables
+        int number = 10;
+        double decimal = 5.5;
+        char letter = 'A';
+        boolean flag = true;
+        String message = "Java Fundamentals";
+        
+        System.out.println("Number: " + number);
+    }
+}
