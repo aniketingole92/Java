0d399d9b9eec67e0ce7a67c3e818b46fb106b787
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-28 20:22:55 +0530

    Create condication.java

---
+++ condication.java
@@ -0,0 +1,16 @@
+// If-else
+if (condition) {
+    // code
+} else {
+    // code
+}
+
+// For loop
+for (int i = 0; i < 10; i++) {
+    // code
+}
+
+// While loop
+while (condition) {
+    // code
+}
