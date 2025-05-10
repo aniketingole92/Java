49cbd5b312c7fac5f3fe5577901a725168fd0a9d
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-10 14:35:04 +0530

    Create operrator.java

---
+++ operrator.java
@@ -0,0 +1,12 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        int sum = a + b;        // Arithmetic: Addition
+        boolean isEqual = (a == b); // Comparison: Equals
+        boolean result = (a > b && b < 10); // Logical: AND
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Is Equal: " + isEqual);
+        System.out.println("Logical Result: " + result);
+    }
+}
