4be88e1d1e859603ead2ec6913bca93118ed6ea5
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-10 14:35:53 +0530

    Create controle.java

---
+++ controle.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("You can vote!");
+        } else {
+            System.out.println("You cannot vote yet.");
+        }
+    }
+}
