cd75e87bd0094515ee771fd2a1f9f1c8295da0d9
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-10 14:34:05 +0530

    Create primitive.java

---
+++ primitive.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 25;           // Whole number
+        double salary = 50000.50; // Decimal number
+        char grade = 'A';       // Single character
+        boolean isStudent = true; // True or false
+        String name = "Rahul";  // Text
+
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
