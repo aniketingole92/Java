8a3ab5fa0c8d9dc0eb632bde01dc89cc1cff8bb8
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-28 18:41:25 +0530

    Create objjctss.java

---
+++ objjctss.java
@@ -0,0 +1,18 @@
+public class Person {
+    String name;
+    int age;
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+    
+    public static void main(String[] args) {
+        Person person = new Person("Amit", 30);
+        person.display();
+    }
+}
