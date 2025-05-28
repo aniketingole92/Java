4a60b6ea88810510c2dab03c7f62d2f1147f3f2f
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-28 18:40:57 +0530

    Create aarra.java

---
+++ aarra.java
@@ -0,0 +1,6 @@
+public class ArraysExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int num : numbers) {
+            System.out.println("Array element: " + num);
+        }
